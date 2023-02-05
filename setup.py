44b3e57659c048ad1270af0186b7206461d683fa
Alexander Sokol <sokol@compatibl.com>
2023-02-05 08:57:22 -0500

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('./README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+with open('./requirements.txt') as requirements_file:
+    requirements = [line.strip() for line in requirements_file.readlines()]
+
+setuptools.setup(
+    name="aemm",
+    version="0.0.1",
+    author="CompatibL",
+    author_email="support@compatibl.com",
+    description="Autoencoder Market Models (AEMM)",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    url="https://www.compatibl.com",
+    packages=setuptools.find_packages(include=('aemm', 'aemm.*'), exclude=['tests', 'tests.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
