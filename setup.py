11302a243cd34fdebd04db0da0464f0e780af977
Alexander Sokol <sokol@compatibl.com>
2023-02-25 14:02:43 -0500

    Add classifiers and project URLs to setup.py.

--- setup.py
+++ setup.py
@@ -7,19 +7,39 @@ with open('./requirements.txt') as requirements_file:
     requirements = [line.strip() for line in requirements_file.readlines()]
 
 setuptools.setup(
-    name="aemm",
-    version="0.0.1",
-    author="CompatibL",
-    author_email="support@compatibl.com",
-    description="Autoencoder Market Models (AEMM)",
+    name='aemm',
+    version='0.0.1',
+    author='CompatibL',
+    author_email='support@compatibl.com',
+    description='Autoencoder Market Models (AEMM)',
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     install_requires=requirements,
-    url="https://www.compatibl.com",
+    url='https://www.compatibl.com',
+    project_urls={
+        'Source Code': 'https://github.com/compatibl/aemm',
+    },
     packages=setuptools.find_packages(include=('aemm', 'aemm.*'), exclude=['tests', 'tests.*']),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+        # Alpha - will attempt to avoid breaking changes but they remain possible
+        'Development Status :: 3 - Alpha',
+
+        # Audience and topic
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+
+        # License
+        'License :: OSI Approved :: Apache Software License',
+
+        # Runs on Python 3.9 and later releases
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+
+        # Operating system
+        'Operating System :: OS Independent',
     ],
 )
