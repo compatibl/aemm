4732d1866edf34802ecb6d888023625b57e9deed
Alexander Sokol <sokol@compatibl.com>
2023-03-31 18:00:56 -0400

    Add linter and formatter settings and scripts.
    (cherry picked from commit 6c555a0189c4a11b51c0a616ff9da5361f6cb0c9)

---
+++ scripts/1_format_and_validate_source.cmd
@@ -0,0 +1,31 @@
+@echo off
+
+echo Switch to parent directory
+pushd ..
+echo.
+
+set target=aemm
+set level=""
+
+echo Activate virtual environment
+call venv\Scripts\activate.bat
+echo.
+
+echo Format using isort
+isort %target% --sp=%level%.isort.cfg
+echo.
+
+echo Format using black
+black %target% --config=%level%pyproject.toml --quiet
+echo.
+
+echo Validate using flake8
+flake8 %target% --config=%level%.flake8
+echo.
+
+echo Deactivate virtual environment
+call venv\Scripts\deactivate.bat
+echo.
+
+echo Return to original directory
+popd
