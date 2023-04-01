19f23ea37ab051d3cd77e5dd6cba57f1eda39c17
Alexander Sokol <sokol@compatibl.com>
2023-04-01 02:07:50 -0400

    Add classifiers and project URLs to setup.py.
    (cherry picked from commit 2b6001ac8a32a352fa138f930144f8e7518cff72)

--- setup.py
+++ setup.py
@@ -7,18 +7,38 @@ with open('./requirements.txt') as requirements_file:
     requirements = [line.strip() for line in requirements_file.readlines()]
 
 setuptools.setup(
-    name="aemm",
-    version="0.0.1",
+    name='aemm',
+    version='0.0.1',
     author="The Project Contributors",
-    description="Autoencoder Market Models (AEMM)",
+    description='Autoencoder Market Models (AEMM)',
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     install_requires=requirements,
     url="https://github.com/compatibl/aemm",
+    project_urls={
+        'Source Code': 'https://github.com/compatibl/aemm',
+    },
     packages=setuptools.find_packages(include=('aemm', 'aemm.*'), exclude=['tests', 'tests.*']),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+        # Alpha - will attempt to avoid breaking changes but they remain possible
+        'Development Status :: 3 - Alpha',
+
+        # Audience and topic
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+
+        # License
+        'License :: OSI Approved :: Apache Software License',
+
+        # Runs on Python 3.9 and later releases
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+
+        # Operating system
+        'Operating System :: OS Independent',
     ],
 )
