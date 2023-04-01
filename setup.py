b66d15a3d9a2421555cd8935a6f303d8be708dc3
Alexander Sokol <sokol@compatibl.com>
2023-04-01 02:38:05 -0400

    Advance version to 0.0.2.
    (cherry picked from commit eb89a87465130c19b32df1e85577ab7bae18ebf0)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('./requirements.txt') as requirements_file:
 
 setuptools.setup(
     name='aemm',
-    version='0.0.1',
+    version='0.0.2',
     author="The Project Contributors",
     description='Autoencoder Market Models (AEMM)',
     long_description=readme,
