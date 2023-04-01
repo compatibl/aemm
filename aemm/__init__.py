387a09a15767b43437380142d1d32b931e3ed493
Alexander Sokol <sokol@compatibl.com>
2023-04-01 02:03:26 -0400

    Directory structure for models and base class for forward rate models.
    (cherry picked from commit 9aa183cf5b78b6a3974aa9090d2fe4908ec3ca0b)

---
+++ aemm/__init__.py
@@ -0,0 +1,5 @@
+from aemm.core.model_key import ModelKey
+from aemm.core.model_type import ModelType
+from aemm.core.model_measure import ModelMeasure
+from aemm.core.model import Model
+from aemm.core.rates.forward_rate.forward_rate_model import ForwardRateModel
