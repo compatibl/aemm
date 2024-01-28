f59143be0eabb84382a229ea60e9402e698e4695
Alexander Sokol <sokol@compatibl.com>
2024-01-27 21:32:53 -0500

    Update setup.py for namespace package structure.

--- setup.py
+++ setup.py
@@ -9,16 +9,20 @@ with open('./install_requirements.txt') as install_requirements:
 setuptools.setup(
     name='aemm',
     version='0.0.2',
-    author="The Project Contributors",
+    author='The Project Contributors',
     description='Autoencoder Market Models (AEMM)',
+    license='Apache Software License',
     long_description=readme,
     long_description_content_type='text/markdown',
     install_requires=install_requires,
-    url="https://github.com/compatibl/aemm",
+    url='https://github.com/compatibl/aemm',
     project_urls={
         'Source Code': 'https://github.com/compatibl/aemm',
     },
-    packages=setuptools.find_packages(include=('aemm', 'aemm.*'), exclude=['tests', 'tests.*']),
+    packages=setuptools.find_namespace_packages(
+        where='.', include=['cl.aemm', 'cl.aemm.*'], exclude=['tests', 'tests.*']
+    ),
+    package_dir={'': '.'},
     classifiers=[
         # Alpha - will attempt to avoid breaking changes but they remain possible
         'Development Status :: 3 - Alpha',
